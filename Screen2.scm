5905d8a825a28834a66c9f6ae57d676f374ee80f
Dep <deaptayanbondopadhay@gmail.com>
2025-04-21 10:10:57 +0530

    Add files via upload

---
+++ Screen2.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"231","Source":"Form","Properties":{"$Name":"Screen2","$Type":"Form","$Version":"31","ActionBar":"True","AppName":"WomenSafetyApp","Title":"Screen2","Uuid":"0"}}
+|#
